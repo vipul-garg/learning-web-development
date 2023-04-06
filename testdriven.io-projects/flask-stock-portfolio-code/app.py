c334b22f6977870fa14cbe2f45459410e686d57e
vipul-linode-vps-01 <am.vipulgarg@gmail.com>
2023-04-06 19:55:01 +0000

    Part 1: Logging Completed.

--- testdriven.io-projects/flask-stock-portfolio-code/app.py
+++ testdriven.io-projects/flask-stock-portfolio-code/app.py
@@ -1,8 +1,25 @@
 from flask import Flask,escape,render_template,request,session,redirect,url_for,flash
 from pydantic import BaseModel,validator,ValidationError
+import logging
+from flask.logging import default_handler
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
+
+# Remove the default logger 
+app.logger.removeHandler(default_handler)
+
 app.secret_key = b'>o\xf5\x10r\xe3\x13\xfa\xd6\xc2UE\xc8\x18\x83\x82\x10\xbfVP\x90\xb5\x16\xf3\x91\x86\xb3\xf2Z\x80Oh'
+
+# Logging Configuration
+file_handler = RotatingFileHandler('flask_stock_portfolio.log',maxBytes=16384,backupCount=20)
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')
+file_handler.setFormatter(file_formatter)
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+# Log that the flask application is starting
+app.logger.info('Starting Flask Stock Portfolio Application')
 
 #---------
 # Helper Classes
@@ -24,6 +41,7 @@ class StockModel(BaseModel):
 
 @app.route('/')
 def index():
+    app.logger.info('Called the index() function.')
     return render_template('index.html')
 
 @app.route('/about',methods=['GET'])
@@ -63,6 +81,7 @@ def add_stock():
             session['number_of_shares'] = stock_data.number_of_shares
             session['purchase_price'] = stock_data.purchase_price
             flash(f"Added new stock ( { stock_data.stock_symbol } )!",'success')
+            app.logger.info(f"Added new stock ( {request.form['stock_symbol']} )!")
 
             return redirect(url_for('list_stocks'))
         except ValidationError as e:
